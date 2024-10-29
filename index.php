7323a57f1a058c6163d7e1ee8f4b623e9dd5fea4
faycalEdderi <faycmx@live.fr>
2024-10-29 11:53:34 +0100

    adding apache homepage link

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
     <p>Choisissez l'application à laquelle vous souhaitez accéder :</p>
     <ul>
         <li><a href="/index.php">Index</a></li>
+        <li><a href="/index.html">Apache</a></li>
         <li><a href="/wordpress">Accéder à WordPress</a></li>
         <li><a href="/phpmyadmin">Accéder à phpMyAdmin</a></li>
+
     </ul>
 </body>
 
